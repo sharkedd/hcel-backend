7ab50835731d8d2e7ed4ac13fa40305e7beb7cc4
Jose Carrillo <jose.carrillo@alumnos.ucn.cl>
2025-11-21 11:22:02 -0300

    Comentarios en las rutas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,7 @@ export class AuthController {
 
   @MessagePattern({ cmd: 'login_user' })
   async login(@Payload() loginDto: LoginDto) {
+    console.log('🔐 Intentando login');
     const user = await this.authService.validateUser(loginDto);
     return this.authService.login(user);
   }
@@ -21,6 +22,7 @@ export class AuthController {
 
   @MessagePattern({ cmd: 'refresh_token' })
   async refreshToken(@Payload() token: string) {
+    console.log('🔄 Refrescando token recibido:', token);
     return this.authService.refreshToken(token);
   }
 }
